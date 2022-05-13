953938cc421043e1c07df5a6752959e55051ecf2
liadhazoot <liadhazoot5@gmail.com>
2022-05-13 17:46:26 +0300

    gender_coach_type ->  GENDER_COACH_TYPE = (         ('M', 'Male'),         ('F', 'Female'),         ('FM', 'Female and Male'),     )
        to_price

--- coach_app/models.py
+++ coach_app/models.py
@@ -5,12 +5,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class CoachDB(models.Model):
+    GENDER_COACH_TYPE = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('FM', 'Female and Male'),
+    )
     person = models.OneToOneField(PersonDB, on_delete=models.CASCADE, related_name='coach_detail')
     rating = models.FloatField(validators=[MaxValueValidator(10.0), MinValueValidator(1.0)], blank=True, default=0)
     number_of_rating = models.IntegerField(blank=True, default=0)
     from_price = models.IntegerField(blank=True, default=0)
     to_price = models.IntegerField(blank=True, default=0)
     is_train_at_home = models.BooleanField(default=False)
+    gender_coach_type = models.CharField(max_length=2, choices=GENDER_COACH_TYPE)
     description = models.CharField(max_length=255, blank=True)
     date_joined = models.DateTimeField(verbose_name='date_joined', auto_now_add=True)
 
