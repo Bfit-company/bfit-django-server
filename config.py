e1d4660ce03a7e7d20be6c838663c9be1a1c8c8e
liadhazoot <liadhazoot5@gmail.com>
2022-07-29 12:58:41 +0300

    chore: change s3 path

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 # S3
-S3_KEY = 'users_images/user={user}/{image_type}/ts_day={ts_day}/{filename}'
+S3_KEY = 'users/user={user}/{image_type}/ts_day={ts_day}/{filename}'
 BUCKET = os.getenv('BUCKET', 'bfit-data-storage')
