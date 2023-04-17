b5ac4925950ab8627132e4803adce9a5e2bc3777
liadhazoot <liadhazoot5@gmail.com>
2023-04-17 23:21:09 +0300

    feat: docker shell

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi -f $(docker images -aq)
 docker build --tag python-django .
-docker run -d --publish 8000:8000 python-django
+docker run -d --restart on-failure:30 --publish 8000:8000 python-django
